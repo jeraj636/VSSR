5a38b4b79f3b85c666c3ba37c5333555b413999d
jeraj636 <jakob.jeraj7@gmail.com>
2024-09-21 00:22:01 +0200

    dela streznik na WIN

--- src/streznik.cpp
+++ src/streznik.cpp
@@ -2,16 +2,21 @@
 
 bool Streznik::zazeni(int port)
 {
+    std::cout << "Odpiranje streznika na portu: " << port << "\n";
+#ifdef LINUX
+
     sockaddr_in naslov_streznika;
     m_st_vseh_odjemalcev = 0;
+
+    //* Odpiranje socketa
     m_vticnik_fd = socket(AF_INET, SOCK_STREAM, 0);
-    bzero((char *)&naslov_streznika, sizeof(naslov_streznika));
 
-    std::cout << "Odpiranje streznika na portu: " << port << "\n";
-
+    //* Nastavljanje neblokiranega načina
     int zastavice = fcntl(m_vticnik_fd, F_GETFL, 0);
     fcntl(m_vticnik_fd, F_SETFL, zastavice | O_NONBLOCK);
 
+    //* Nastavljanje podatkov o strezniku
+    bzero((char *)&naslov_streznika, sizeof(naslov_streznika));
     naslov_streznika.sin_family = AF_INET;
     naslov_streznika.sin_addr.s_addr = INADDR_ANY;
     naslov_streznika.sin_port = htons(port);
@@ -21,16 +26,37 @@ bool Streznik::zazeni(int port)
         std::cout << "Napaka pri bindanju naslova streznika!\n";
         return false;
     }
+
+#endif
+
+#ifdef WINDOWS
+    SOCKADDR_IN naslov_streznika;
+    WSAStartup(MAKEWORD(2, 0), &m_WSA_data);
+    m_vticnik = socket(AF_INET, SOCK_STREAM, 0);
+
+    naslov_streznika.sin_addr.s_addr = INADDR_ANY;
+    naslov_streznika.sin_family = AF_INET;
+    naslov_streznika.sin_port = htons(port);
+
+    u_long neblokiran = 1;
+    ioctlsocket(m_vticnik, FIONBIO, &neblokiran);
+
+    if (bind(m_vticnik, (SOCKADDR *)&naslov_streznika, sizeof(naslov_streznika)) == SOCKET_ERROR)
+    {
+        std::cout << "Napaka pri bindanju naslova streznika!\n";
+        return false;
+    }
+#endif
+
     m_streznik_tece = true;
     m_nit_za_poslusanje = std::thread(poslusaj);
-    // m_nit_za_poslusanje.detach();
     std::cout << "Streznik zagnan!\n";
     return true;
 }
-
 void Streznik::poslusaj()
 {
     std::cout << "Poslusam!\n";
+#ifdef LINUX
     listen(m_vticnik_fd, 20);
     while (m_streznik_tece)
     {
@@ -51,11 +77,35 @@ void Streznik::poslusaj()
         std::thread nit(vzdrzuj_povezavo, odjemalec);
         nit.detach();
     }
+#endif
+#ifdef WINDOWS
+    listen(m_vticnik, 20);
+    while (m_streznik_tece)
+    {
+        Odjemalec_zs *odjemalec = new Odjemalec_zs;
+
+        odjemalec->m_odjemalec_vel = sizeof(odjemalec->m_naslov_odjemalca);
+        odjemalec->m_nov_vticnik = accept(m_vticnik, (sockaddr *)&odjemalec->m_naslov_odjemalca, &odjemalec->m_odjemalec_vel);
+        if (odjemalec->m_nov_vticnik == INVALID_SOCKET || WSAGetLastError() == WSAEWOULDBLOCK)
+        {
+            delete odjemalec;
+            continue;
+        }
+
+        std::cout << "Nova povezava: " << m_st_vseh_odjemalcev << "\n";
+        odjemalec->odjemalec_id = m_st_vseh_odjemalcev++;
+        odjemalci.push_back(odjemalec);
+        std::thread nit(vzdrzuj_povezavo, odjemalec);
+        nit.detach();
+    }
+
+#endif
     std::cout << "Konec poslusanje!\n";
 }
 
 void Streznik::vzdrzuj_povezavo(Odjemalec_zs *odjemalec)
 {
+#ifdef LINUX
     int zastavice = fcntl(odjemalec->m_nov_vticnik_fd, F_GETFL, 0);
     if (zastavice == -1)
     {
@@ -96,12 +146,46 @@ void Streznik::vzdrzuj_povezavo(Odjemalec_zs *odjemalec)
     {
         m_nit_za_poslusanje.join();
     }
+#endif
 
+#ifdef WINDOWS
+    while (m_streznik_tece)
+    {
+        char buffer[256];
+        int n = recv(odjemalec->m_nov_vticnik, buffer, 255, 0);
+        if (n <= 0 || buffer[0] == '\n' || buffer[0] == '\r')
+            continue;
+        if (buffer[0] == 'k')
+            break;
+        if (n > 0)
+            std::cout << buffer << "\n";
+        std::cout << "Cakam:  " << odjemalec->odjemalec_id << "  " << n << "\n";
+    }
+    if (!m_streznik_tece)
+    {
+        closesocket(odjemalec->m_nov_vticnik);
+        for (int i = 0; i < odjemalci.size(); i++)
+        {
+            if (odjemalec->odjemalec_id == odjemalci[i]->odjemalec_id)
+            {
+                std::swap(odjemalci[i], odjemalci.back());
+                delete odjemalci.back();
+                odjemalci.pop_back();
+                break;
+            }
+        }
+    }
+    if (m_nit_za_poslusanje.joinable())
+    {
+        m_nit_za_poslusanje.join();
+    }
+#endif
     std::cout << "Konec povezave!\n";
 }
 
 void Streznik::ugasni()
 {
+#ifdef LINUX
     m_streznik_tece = false;
     if (odjemalci.size() == 0)
         m_nit_za_poslusanje.join();
@@ -114,4 +198,20 @@ void Streznik::ugasni()
 
     close(m_vticnik_fd);
     std::cout << "Konec streznik!\n";
+#endif
+#ifdef WINDOWS
+    m_streznik_tece = false;
+    if (odjemalci.size() == 0)
+        m_nit_za_poslusanje.join();
+    while (odjemalci.size() != 0)
+    {
+        closesocket(odjemalci.back()->m_nov_vticnik);
+        delete odjemalci.back();
+        odjemalci.pop_back();
+    }
+
+    closesocket(m_vticnik);
+    WSACleanup();
+    std::cout << "Konec streznik!\n";
+#endif
 }
--- src/streznik.h
+++ src/streznik.h
@@ -1,18 +1,26 @@
 #ifndef STREZNIK_H
 #define STREZNIK_H
-
+#define WINDOWS
+#ifdef LINUX
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#endif
 
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <vector>
-#include <thread>
-#include <iostream>
+#ifdef WINDOWS
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
+
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <iostream>
+#include <thread>
+
 class Streznik;
 class Odjemalec_zs
 {
@@ -20,9 +28,16 @@ public:
     friend class Streznik;
 
 protected:
+#ifdef LINUX
     socklen_t m_odjemalec_vel;
     sockaddr_in m_naslov_odjemalca;
     int m_nov_vticnik_fd;
+#endif LINUX
+#ifdef WINDOWS
+    SOCKET m_nov_vticnik;
+    SOCKADDR_IN m_naslov_odjemalca;
+    int m_odjemalec_vel;
+#endif
     int odjemalec_id;
 };
 
@@ -33,12 +48,18 @@ public:
     static void ugasni();
 
 private:
-    static inline int m_vticnik_fd;
     static inline std::thread m_nit_za_poslusanje;
-    static inline bool m_streznik_tece;
     static inline std::vector<Odjemalec_zs *> odjemalci;
     static inline int m_st_vseh_odjemalcev;
+    static inline bool m_streznik_tece;
+#ifdef LINUX
+    static inline int m_vticnik_fd;
+#endif
+#ifdef WINDOWS
+    static inline WSADATA m_WSA_data;
+    static inline SOCKET m_vticnik;
 
+#endif
 private:
     static void vzdrzuj_povezavo(Odjemalec_zs *odjemalec);
     static void poslusaj();
