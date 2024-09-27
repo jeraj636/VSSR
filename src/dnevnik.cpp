9cb6425692f350b45b9611d2638f1d743c698ee9
jeraj636 <jakob.jeraj7@gmail.com>
2024-09-27 21:47:55 +0200

    dela dnevnik na WIN

--- src/dnevnik.cpp
+++ src/dnevnik.cpp
@@ -12,88 +12,88 @@
 void napaka(const char *sporocilo, ...)
 {
 #ifdef DEBUG
-    if (STOPNJA_SPOROCANJA >= 1)
-    {
+        if (STOPNJA_SPOROCANJA >= 1)
+        {
 #ifdef LINUX
-        printf("\033[0;31m"); // rdeca
+                printf("\033[0;31m"); // rdeca
 #endif
 #ifdef WINDOWS
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED); // Zeleni tekst
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, FOREGROUND_RED); // Zeleni tekst
 #endif
-        va_list parametri;
-        va_start(parametri, sporocilo);
-        vprintf(sporocilo, parametri);
-        va_end(parametri);
-        fflush(stdout);
+                va_list parametri;
+                va_start(parametri, sporocilo);
+                vprintf(sporocilo, parametri);
+                va_end(parametri);
+                fflush(stdout);
 #ifdef LINUX
-        printf("\033[0m");
+                printf("\033[0m");
 #endif
 #ifdef WINDOWS
-        SetConsoleTextAttribute(hConsole, FOREGROUND_WHITE); // Resetiranje tekst
+                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY); // Resetiranje tekst
 #endif
-    }
+        }
 #endif
 }
 void opozorilo(const char *sporocilo, ...)
 {
 #ifdef DEBUG
-    if (STOPNJA_SPOROCANJA >= 2)
-    {
+        if (STOPNJA_SPOROCANJA >= 2)
+        {
 #ifdef LINUX
-        printf("\033[0;33m"); // rumena
+                printf("\033[0;34m"); // modra
 #endif
 #ifdef WINDOWS
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole, FOREGROUND_YELOW); // Zeleni tekst
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE); // Zeleni tekst
 #endif
-        va_list parametri;
-        va_start(parametri, sporocilo);
-        vprintf(sporocilo, parametri);
-        va_end(parametri);
-        fflush(stdout);
+                va_list parametri;
+                va_start(parametri, sporocilo);
+                vprintf(sporocilo, parametri);
+                va_end(parametri);
+                fflush(stdout);
 #ifdef LINUX
-        printf("\033[0m");
+                printf("\033[0m");
 #endif
 #ifdef WINDOWS
-        SetConsoleTextAttribute(hConsole, FOREGROUND_WHITE); // Resetiranje tekst
+                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY); // Resetiranje tekst
 #endif
-    }
+        }
 #endif
 }
 void sporocilo(const char *sporocilo, ...)
 {
 #ifdef DEBUG
-    if (STOPNJA_SPOROCANJA >= 3)
-    {
+        if (STOPNJA_SPOROCANJA >= 3)
+        {
 #ifdef LINUX
-        printf("\033[0;32m"); // zelena
+                printf("\033[0;32m"); // zelena
 #endif
 #ifdef WINDOWS
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN); // Zeleni tekst
+                HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+                SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN); // Zeleni tekst
 #endif
-        va_list parametri;
-        va_start(parametri, sporocilo);
-        vprintf(sporocilo, parametri);
-        va_end(parametri);
-        fflush(stdout);
+                va_list parametri;
+                va_start(parametri, sporocilo);
+                vprintf(sporocilo, parametri);
+                va_end(parametri);
+                fflush(stdout);
 #ifdef LINUX
-        printf("\033[0m");
+                printf("\033[0m");
 #endif
 #ifdef WINDOWS
-        SetConsoleTextAttribute(hConsole, FOREGROUND_WHITE); // Resetiranje tekst
+                SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY); // Resetiranje tekst
 #endif
-    }
+        }
 #endif
 }
 void izpis(const char *sporocilo, ...)
 {
 #ifdef DEBUG
-    va_list parametri;
-    va_start(parametri, sporocilo);
-    vprintf(sporocilo, parametri);
-    va_end(parametri);
-    fflush(stdout);
+        va_list parametri;
+        va_start(parametri, sporocilo);
+        vprintf(sporocilo, parametri);
+        va_end(parametri);
+        fflush(stdout);
 #endif
 }
