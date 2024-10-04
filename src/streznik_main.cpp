e9c382026cba51db66ac48b73d384cc24dc77e13
Jakob <jakob-jeraj7@gmail.com>
2024-10-04 22:07:42 +0200

    samostojen streznik

---
+++ src/streznik_main.cpp
@@ -0,0 +1,10 @@
+#include "streznik.h"
+#include "dnevnik.h"
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+        napaka("Ni podane st vrat!\n");
+    Streznik::zazeni(std::atoi(argv[1]));
+    while (1)
+        ;
+}
